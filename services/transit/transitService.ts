ee9affb02cd1fed4f1fb31048a9aee99f4403d21
lucas pereira <lucasferrari4@gmail.com>
2025-04-11 15:52:58 -0600

    other interesting real time apis

--- services/transit/transitService.ts
+++ services/transit/transitService.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import GtfsRealtimeBindings from 'gtfs-realtime-bindings';
 import { Vehicle } from './types';
 import { determineVehicleType } from '../../utils/vehicleUtils';
+import * as fs from 'fs/promises';
 
 export const fetchTransitData = async (): Promise<Vehicle[]> => {
     try {
@@ -10,10 +11,29 @@ export const fetchTransitData = async (): Promise<Vehicle[]> => {
             url: 'https://data.calgary.ca/download/am7c-qe3u/application%2Foctet-stream',
             responseType: 'arraybuffer',
         });
+        // const serviceAlerts = await axios({
+        //     method: 'get',
+        //     url: 'https://data.calgary.ca/download/jhgn-ynqj/application%2Foctet-stream',
+        //     responseType: 'arraybuffer',
+        // });
+        // const tripUpdates = await axios({
+        //     method: 'get',
+        //     url: 'https://data.calgary.ca/download/gs4m-mdc2/application%2Foctet-stream',
+        //     responseType: 'arraybuffer',
+        // });
 
         const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
             new Uint8Array(response.data)
         );
+        // const serviceAlertsFeed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+        //     new Uint8Array(serviceAlerts.data)
+        // );
+        // const tripUpdatesFeed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+        //     new Uint8Array(tripUpdates.data)
+        // );
+        await writeFeedToFile('feed.json', response.data);
+        // await writeFeedToFile('serviceAlerts.json', serviceAlerts.data);
+        // await writeFeedToFile('tripUpdates.json', tripUpdates.data);
 
         return feed.entity
             .filter(entity => entity.vehicle && entity.vehicle.vehicle && entity.vehicle.position)
@@ -38,3 +58,17 @@ export const fetchTransitData = async (): Promise<Vehicle[]> => {
 export const transitService = {
     fetchRealTimeData: fetchTransitData
 };
+
+
+export async function writeFeedToFile(filePath: string, data: any): Promise<void> {
+    try {
+      const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+        new Uint8Array(data)
+      );
+      const feedJson = JSON.stringify(feed, null, 2);
+      await fs.writeFile(filePath, feedJson);
+      console.log(`Feed written to ${filePath}`);
+    } catch (error) {
+      console.error(`Error writing feed to ${filePath}:`, error);
+    }
+  }
