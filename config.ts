6f708e9f0e7c45f86ad972a284a8e96428a696fe
lucas pereira <lucasferrari4@gmail.com>
2025-04-14 16:18:24 -0600

    ip for laptop

--- config.ts
+++ config.ts
@@ -1,7 +1,9 @@
 // config.ts
 import { Platform } from 'react-native';
 
-const DEV_MACHINE_IP = '10.0.0.177'; // Your development machine's IP
+//mateus computer : 10.0.0.177
+// inceptionu laptop : 10.44.22.28
+const DEV_MACHINE_IP = '10.44.22.28'; // Your development machine's IP
 const PROD_API_URL = 'https://your-production-api.com'; // Your production API URL
 
 export const TRIP_MAPPING_API_URL = __DEV__ 
